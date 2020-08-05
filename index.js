03c66e0041a8e221071a5db959731bf83a382c50
Onur Çipe <onur@dolphinos.com>
2020-08-05 11:21:27 +0300

    fix (core): add missing export for db safe

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const Cell = require("./lib/core/Cell");
 const Schema = require("./lib/db/Schema");
 const DbService = require("./lib/db/DbService");
+const DbSafe = require("./lib/safes/DbSafe");
 
 module.exports = {
   Cell,
   Schema,
-  DbService
+  DbService,
+  DbSafe
 };
