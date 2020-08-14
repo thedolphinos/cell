e657795d91ad5a7cdde33be372af0e99a1172ab7
Onur Çipe <onur@dolphinos.com>
2020-08-14 16:52:42 +0300

    feature (db): schema definition

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const Cell = require("./lib/core/Cell");
+const DataType = require("./lib/db/DataType");
 const Schema = require("./lib/db/Schema");
 const DbService = require("./lib/db/DbService");
 const DbSafe = require("./lib/safes/DbSafe");
 
 module.exports = {
   Cell,
+  DataType,
   Schema,
   DbService,
   DbSafe
