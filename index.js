5be04e953e92cda20810f1165a5ac67de12ca686
Onur Çipe <onur@dolphinos.com>
2020-08-20 14:17:14 +0300

    fix: add missing exports

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 "use strict";
 
 const Cell = require("./lib/core/Cell");
+const DbSafe = require("./lib/safes/DbSafe");
 const DataType = require("./lib/db/DataType");
 const Schema = require("./lib/db/Schema");
 const DbService = require("./lib/db/DbService");
-const DbSafe = require("./lib/safes/DbSafe");
+const RouteHelper = require("./lib/helpers/RouteHelper");
+const ControllerHelper = require("./lib/helpers/ControllerHelper");
+const AuthController = require("./lib/controllers/AuthController");
 
 module.exports = {
   Cell,
+
+  DbSafe,
+
   DataType,
   Schema,
   DbService,
-  DbSafe
+
+  RouteHelper,
+  ControllerHelper,
+  AuthController
 };
