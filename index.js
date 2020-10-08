53b851878392719ca994070823e47127d93ca00b
Onur Çipe <onur@dolphinos.com>
2020-10-08 12:25:17 +0300

    feature (service): add bearer to hooks

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-"use strict";
+const Error = require("@thedolphinos/error4js"); // this is exported due to node's duplicate module imports which causes reference problems during instanceof checks.
 
 const Cell = require("./lib/core/Cell");
-const Error = require("@thedolphinos/error4js"); // this is exported due to node's duplicate module imports which causes reference problems during instanceof checks.
+const Logger = require("./lib/core/Logger");
+const Validator = require("./lib/core/Validator");
+
 const DbSafe = require("./lib/safes/DbSafe");
 const LanguageSafe = require("./lib/safes/LanguageSafe");
+
+const SessionManager = require("./lib/db/SessionManager");
 const Schema = require("./lib/db/Schema");
 const DbOperation = require("./lib/db/DbOperation");
-const SessionManager = require("./lib/db/SessionManager");
+
 const DbService = require("./lib/services/DbService");
 const ApplicationService = require("./lib/services/ApplicationService");
 const ControllerService = require("./lib/services/ControllerService");
+
 const Controller = require("./lib/core/Controller");
 const AuthController = require("./lib/controllers/AuthController");
 const CrudController = require("./lib/controllers/CrudController");
+
 const RouteHelper = require("./lib/helpers/RouteHelper");
 
 module.exports = {
-  Cell,
   Error,
+
+  Cell,
+  Logger,
+  Validator,
 
   DbSafe,
   LanguageSafe,
 
+  SessionManager,
   Schema,
   DbOperation,
-  SessionManager,
 
   DbService,
   ApplicationService,
